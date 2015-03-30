b5ad372cfa1177cff491079ba3d6695a33094274
epatter1 <elishajp20@gmail.com>
2015-03-30 13:49:19 -0400

    Room class

---
+++ After School/src/Room.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+
+public class Room {
+	ArrayList<Integer> roomID = new ArrayList<Integer>();
+	protected int roomLocation;
+
+	public static void main(String stuff)
+	{
+		Room roomID = new Room();
+		System.out.println(roomID);
+	
+	}
+}
