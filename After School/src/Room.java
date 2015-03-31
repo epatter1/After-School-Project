af8bb06a069387b8c7c7574badd7ebb9ab9c2480
epatter1 <elishajp20@gmail.com>
2015-03-31 16:15:06 -0400

    Finally!

--- After School/src/Room.java
+++ After School/src/Room.java
@@ -5,7 +5,7 @@ public class Room {
 	ArrayList<Integer> roomID = new ArrayList<Integer>();
 	protected int roomLocation;
 
-	public static void main(String stuff)
+	public static void main(String[] stuff)
 	{
 		Room roomID = new Room();
 		System.out.println(roomID);
