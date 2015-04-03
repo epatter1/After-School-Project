b7c884f7e008ac7bdc1cd94603219c4a58189867
epatter1 <elishajp20@gmail.com>
2015-04-03 13:19:42 -0400

    Room class sample

--- After School/src/Room.java
+++ After School/src/Room.java
@@ -1,14 +1,52 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class Room {
-	ArrayList<Integer> roomID = new ArrayList<Integer>();
-	protected int roomLocation;
+	// put characters and items into rooms
 
-	public static void main(String[] stuff)
+   	ArrayList<Integer> npc = new ArrayList<Integer>();
+   	ArrayList<Room> monster = new ArrayList<Room>();
+   	ArrayList<Room> item = new ArrayList<Room>();
+   	
+ public String searchRoom()
 	{
-		Room roomID = new Room();
-		System.out.println(roomID);
-	
+		Scanner scan= new Scanner(System.in);
+// if room contains monster, print monster
+		return "the room contains ";
+		
+// if room contains item, print item
+// if room contains npc, print npc
+		
 	}
+	
+	private int room1;
+
+	   	
+		public int assignRoom()
+		{
+			
+			npc.add(room1);
+			int count = 0;
+			for(int i = 0; i < npc.size()- i; i++)
+			{
+
+				npc.add(room1);
+				count++;
+				System.out.println("Room 1 contains " + room1);
+				
+			}
+			return count;
+		}
+	
+
+		public static void main (String[] stuff)
+		{
+			Room room = new Room();
+			System.out.println(room.assignRoom());
+		}
 }
+
+
+
+
